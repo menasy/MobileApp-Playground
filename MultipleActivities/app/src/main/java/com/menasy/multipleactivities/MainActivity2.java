8496a9ea25daa9bf98a371810e07fe768b8d3bbf
menasy <mehmetnasim42@gmail.com>
2024-09-10 22:43:06 +0300

    Multiple Activity Usage

---
+++ MultipleActivities/app/src/main/java/com/menasy/multipleactivities/MainActivity2.java
@@ -0,0 +1,43 @@
+package com.menasy.multipleactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        TextView textView;
+        String newPlayer;
+
+        Intent intent;
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textView = findViewById(R.id.textView2);
+        intent = getIntent();
+        newPlayer = intent.getStringExtra("player");
+        textView.setText("Your answer: " + newPlayer);
+    }
+    public void changeScreen(View view)
+    {
+
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
